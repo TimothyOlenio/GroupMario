7731298b16daa200702d750a0221195289f2cb31
IronFatalis <52928832+IronFatalis@users.noreply.github.com>
2019-07-19 14:07:35 -0400

    Files given in class
    All tile sets given in class and very basic startup of world 1-1.

---
+++ Mario/Maps/World_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="mario3Tiles" tilewidth="18" tileheight="18" tilecount="512" columns="32">
+ <image source="../Images/mario3Tiles.png" width="576" height="288"/>
+</tileset>
