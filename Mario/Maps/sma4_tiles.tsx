c136196d5eea2b4a93730034f514735c15d16b72
eitrunix <52928831+eitrunix@users.noreply.github.com>
2019-07-19 15:41:36 -0400

    Overworld Map 90% Done
    Missing the border of the map, not sure if we needto find it or if we can use an image to "cover" over it?

---
+++ Mario/Maps/sma4_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="sma4_tiles" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1482" columns="57">
+ <image source="../Images/sma4_tiles.png" width="982" height="452"/>
+</tileset>
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # GroupMario
+
+//-- Change Log
+
+//-- The Overworld Map was mostly created, we are missing the outter boarders of the map currently, something that can be fixed later.
