385ae7b5bfccf85f8003caccb3070067dfad133c
IronFatalis <52928832+IronFatalis@users.noreply.github.com>
2019-07-21 00:25:09 -0400

    Tile work 1-1 90%
    This tile work for level 1-1 almost complete. Coins and blocks animated. All layers complete except for main background.

---
+++ Mario/Maps/mario3Tiles.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="mario3Tiles" tilewidth="16" tileheight="16" spacing="2" margin="2" tilecount="512" columns="32">
+ <image source="../Images/mario3Tiles.png" width="576" height="288"/>
+ <tile id="228">
+  <animation>
+   <frame tileid="228" duration="200"/>
+   <frame tileid="229" duration="200"/>
+   <frame tileid="230" duration="200"/>
+   <frame tileid="231" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="352">
+  <animation>
+   <frame tileid="352" duration="200"/>
+   <frame tileid="353" duration="200"/>
+   <frame tileid="354" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
