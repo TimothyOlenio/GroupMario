4c7a7b8c30291a97d8a80842800df39492806293
eitrunix <52928831+eitrunix@users.noreply.github.com>
2019-07-22 23:14:41 -0400

    Added Basic movement code to the entities.js, added keyboard controls for LEFT, RIGHT, JUMP.
    Added Movement code and keyboard controls, as well as added an object and basic collision to the first level to test movement, as soon as keyboard controls are added the game stops running.   All new code is commented out as it currently does nothing.

---
+++ Mario_Game/data/map/mario_walk_right.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="mario_walk_right" tilewidth="20" tileheight="20" tilecount="3" columns="3">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="../img/mario_walk_right.png" width="60" height="20"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="5" y="5" width="10" height="15"/>
+  </objectgroup>
+ </tile>
+</tileset>
--- Mario_Game/js/game.js
+++ Mario_Game/js/game.js
@@ -25,15 +25,26 @@ var game = {
         me.loader.preload(game.resources, this.loaded.bind(this));
     },
 
-    // Run on game resources loaded.
+// -- Run on game resources loaded.
+    
     "loaded" : function () {
         me.state.set(me.state.MENU, new game.TitleScreen());
         me.state.set(me.state.PLAY, new game.PlayScreen());
 
-        // add our player entity in the entity pool
+// -- add our player entity in the entity pool
+        
         me.pool.register("mainPlayer", game.PlayerEntity);
+        
+// -- Enable Keyboard 
+        
+      //  me.input.bindKey(me.input.KEY.LEFT,  "left");
+      //  me.input.bindkey(me.input.KEY.RIGHT, "right");
+      //  me.input.bindkey(me.input.KEY.X, "jump");
+      //  me.input.bindkey(me.input.KEY.UP, "jump");
+      //  me.input.bindkey(me.input.KEY.SPACE, "jump"); 
 
-        // Start the game.
+// -- Start the game.
+        
         me.state.change(me.state.MENU);
     }
 };
