2557a98a7446f808fa9331f52ce7a639fd12201e
eitrunix <52928831+eitrunix@users.noreply.github.com>
2019-07-24 12:01:57 -0400

    Added Goomba Basic coding, Collectable coin basic coding
    --Added a goombaEntity to the object pool,
    
    --added CoinEnitity to the Object pool,
    
    ---Attempted adding Jumping Animation, didnt work.
    
    --Added Collision to Enemies, Currently mairo will flicker, Mario will bounce off the Object repeatedly, Enemies dont "Die" --- Still very buggy, passing it off to Tim and Jay
    
    ---Added Collision to Coins, They will dissapear and the player gains 250 score. other collectables still need backend code and animation. Pass off to Ryan
    
    -- added some sprite sheets, might get used?

--- Mario_Game/js/game.js
+++ Mario_Game/js/game.js
@@ -37,6 +37,8 @@ var game = {
 // -- add our player entity in the entity pool
         
         me.pool.register("mainPlayer", game.PlayerEntity);
+        me.pool.register("CoinEntity", game.CoinEntity);
+        me.pool.register("GoombaEntity", game.GoombaEntity);
         
 // -- Enable Keyboard 
         
