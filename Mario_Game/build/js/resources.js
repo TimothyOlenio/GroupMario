665e84f13b2e34852b4c9be2d927c96f6494f2f0
Tim Olenio <52928854+TimothyOlenio@users.noreply.github.com>
2019-07-24 08:24:03 -0700

    Create resources.js

---
+++ Mario_Game/build/js/resources.js
@@ -0,0 +1,107 @@
+game.resources = [
+    {
+        "name": "00001",
+        "type": "image",
+        "src": "data/img/00001.png"
+    },
+    {
+        "name": "1-1",
+        "type": "image",
+        "src": "data/img/1-1.png"
+    },
+    {
+        "name": "mario_walk_right",
+        "type": "image",
+        "src": "data/img/mario_walk_right.png"
+    },
+    {
+        "name": "mario3Tiles",
+        "type": "image",
+        "src": "data/img/mario3Tiles.png"
+    },
+    {
+        "name": "sma4_tiles",
+        "type": "image",
+        "src": "data/img/sma4_tiles.png"
+    },
+    {
+        "name": "SMBWorld1Map",
+        "type": "image",
+        "src": "data/img/SMBWorld1Map.png"
+    },
+    {
+        "name": "Super_Mario_3_Mario_World1_Map1",
+        "type": "image",
+        "src": "data/img/Super_Mario_3_Mario_World1_Map1.png"
+    },
+    {
+        "name": "tilesSMAS",
+        "type": "image",
+        "src": "data/img/tilesSMAS.png"
+    },
+    {
+        "name": "Mario_1-1",
+        "type": "tmx",
+        "src": "data/map/Mario_1-1.tmx"
+    },
+    {
+        "name": "Mario_Overworld1",
+        "type": "tmx",
+        "src": "data/map/Mario_Overworld1.tmx"
+    },
+    {
+        "name": "00001",
+        "type": "tsx",
+        "src": "data/map/00001.tsx"
+    },
+    {
+        "name": "Level1",
+        "type": "tsx",
+        "src": "data/map/Level1.tsx"
+    },
+    {
+        "name": "mario_walk_right",
+        "type": "tsx",
+        "src": "data/map/mario_walk_right.tsx"
+    },
+    {
+        "name": "mario3Tiles",
+        "type": "tsx",
+        "src": "data/map/mario3Tiles.tsx"
+    },
+    {
+        "name": "sma4_tiles",
+        "type": "tsx",
+        "src": "data/map/sma4_tiles.tsx"
+    },
+    {
+        "name": "SMBWorld1Map_Mario",
+        "type": "tsx",
+        "src": "data/map/SMBWorld1Map_Mario.tsx"
+    },
+    {
+        "name": "SMBWorld1Map_Obj",
+        "type": "tsx",
+        "src": "data/map/SMBWorld1Map_Obj.tsx"
+    },
+    {
+        "name": "SMBWorld1Map",
+        "type": "tsx",
+        "src": "data/map/SMBWorld1Map.tsx"
+    },
+    {
+        "name": "Super_Mario_3_Mario_World1_Map1",
+        "type": "tsx",
+        "src": "data/map/Super_Mario_3_Mario_World1_Map1.tsx"
+    },
+    {
+        "name": "tilesSMAS",
+        "type": "tsx",
+        "src": "data/map/tilesSMAS.tsx"
+    },
+    {
+        "name": "World_1",
+        "type": "tsx",
+        "src": "data/map/World_1.tsx"
+    }
+];
