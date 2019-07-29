c77cd1f099eb20e94ff59b79c46765af1e1bb9a7
Jeff Oldfield <52928831+eitrunix@users.noreply.github.com>
2019-07-29 08:09:35 -0400

    stupid folder

--- Mario/Maps/00001.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="00001" tilewidth="18" tileheight="18" margin="6" tilecount="4" columns="2">
- <grid orientation="orthogonal" width="16" height="16"/>
- <image source="../Images/00001.png" width="43" height="44"/>
-</tileset>
--- Mario/Maps/Level1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="1-1" tilewidth="16" tileheight="16" tilecount="8010" columns="178">
- <image source="../Images/1-1.png" width="2848" height="720"/>
-</tileset>
