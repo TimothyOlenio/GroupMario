58f7ac61bde3fe08692c59735bf100f2ff3b71e8
RiggsbyMortis <52928834+RiggsbyMortis@users.noreply.github.com>
2019-07-31 10:06:13 -0400

    koopas now flip when jump direction when they hit x limit
    hoo boy still got a lot to do

--- Mario_Game/js/game.js
+++ Mario_Game/js/game.js
@@ -45,6 +45,7 @@ var game = {
         me.pool.register("LeafEntity", game.LeafEntity);
         me.pool.register("KoopaEntity", game.KoopaEntity);
         me.pool.register("WingGoombaEntity", game.WingGoombaEntity);
+        me.pool.register("WingKoopaEntity", game.WingKoopaEntity);
         
         me.pool.register("PiranhaEntity", game.PiranhaEntity);
         me.pool.register("RedFirePiranhaEntity", game.RedFirePiranhaEntity);
